c1a0d3578ea984bfd821e774eb7bc92dab42f6fb
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-02-17 19:47:03 +0530

    new change

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const http = require("http");   // HTTP package for server
-
+const fs = require("fs");
 // Create the server
 const myServer = http.createServer((req, res) => {
     console.log("New request!");
-
-    switch(req.url) {
-        case '/': 
-            res.end("Welcome to home page");
-            break;
-        case '/about': 
-            res.end("Hey I am Kushagra");
-            break;
-        default: 
-            res.end("404 Not Found");
-    }
+    const log=`New req at -> ${req.url}\n`;
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url) {
+            case '/': 
+                res.end("Welcome to home page");
+                break;
+            case '/about': 
+                res.end("Hey I am Kushagra");
+                break;
+            default: 
+                res.end("404 Not Found");
+        }
+    });
 });
 
 // Listen on port 8000
