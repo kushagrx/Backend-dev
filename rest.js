4c567a82780001304fec5ace337cbbb26e485421
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-02-21 14:39:38 +0530

    all the basic API routes

--- rest.js
+++ rest.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
-const app=express();
-const PORT=5000;
+const app = express();
+const PORT = 5000;
 
-//routes
-app.get("/users",(req,res)=>{
+// REST API routes
+app.get("/users", (req, res) => {
     return res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    return res.json(user);
+});
+
+app.post("/users/:id",(req,res)=>{
+    //to do: create new user
+    return res.json({status:"pending"});
 })
-app.listen(5000,()=>console.log("Server started"));
+app.patch("/users/:id",(req,res)=>{
+    //to do: update new user
+    return res.json({status:"pending"});
+})
+app.delete("/users/:id",(req,res)=>{
+    //to do: delete user
+    return res.json({status:"pending"});
+})
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
